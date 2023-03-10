3634c560cd586bd85c23483870e2be06a8582c87
MyoMinLatt <myominlatt.sbs@gmail.com>
2023-03-10 13:14:10 +0630

    signupRoute

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,19 @@ const express = require("express");
 
 const authRouter = express.Router();
 
-authRouter.get('/user', (req, res) => {
-    res.json({ route: "Good Morning" });
+authRouter.post('/api/signup', (req, res) => {
+    const { name, email, password } = req.body;
+    // console.log(req.body);
+    // get the data from client
+    // post that data in database
+    // return that dtat to the user
 })
+
+
+
+
+// middleware
+// authRouter.get('/user', (req, res) => {
+//     res.json({ route: "Good Morning" });
+// })
 module.exports = authRouter;
