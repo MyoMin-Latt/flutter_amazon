811c3f04f5e6317d4c4760e67af07f69dc3c7007
MyoMinLatt <myominlatt.sbs@gmail.com>
2023-03-10 15:14:00 +0630

    userModel

---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        required: true,
+        type: String,
+        trim: true,
+    },
+    email: {
+        required: true,
+        type: String,
+        trim: true,
+        validate: {
+            validator: (value) => {
+                const re = /^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{10,16}$/;
+                return value.match(re);
+            },
+            message: "Please enter a valid email address",
+        },
+    },
+    password: {
+        required: true,
+        type: String,
+    },
+    address: {
+        type: String,
+        default: "",
+    },
+    type: {
+        type: String,
+        default: "user",
+    },
+    // cart
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const User = require("./models/user");
 
 const authRouter = express.Router();
 
 authRouter.post('/api/signup', (req, res) => {
     const { name, email, password } = req.body;
+    User
     // console.log(req.body);
     // get the data from client
     // post that data in database // weak password 6 char, same account with email
